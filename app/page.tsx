94fbd8e97692e6d8af0b39559e8605d0f97b8c19
Amartya Sinha <amartua2002@gmail.com>
2024-02-05 06:51:13 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
       {isTabletOrMobile ? (
         <div className="flex justify-center items-center h-screen">
          <div className=" p-6 ">
-      <h1 className="text-2xl font-semibold mb-2">Mobile View is <span className="italic text-red-500">almost ready</span>.</h1>
-      <p className=" text-zinc-500 font-sans ">
+      <h1 className="text-3xl font-semibold mb-2">Mobile View is <span className="italic text-red-500">almost ready</span>.</h1>
+      <p className=" text-zinc-500 font-sans dark:text-zinc-400">
         We're working on a fantastic mobile experience for you. For now, please enjoy our website on your desktop browser.
       </p>
     </div>
